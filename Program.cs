043997972f01fced166074b91b7e69b01cd2e146
SemenovAleksandr <a_v_semenov@mail.ru>
2022-08-26 23:48:12 +0500

    Семинар по C#. Домашнее задание 1. Добавил Задачу 2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,43 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// -------- Задача 1 ---------------
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.WriteLine("Введите первое число: ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int secNum = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNum > secNum)
+// {
+//     Console.WriteLine("max = " + firstNum + "; " + "min = " + secNum);
+// }
+// else Console.WriteLine("max = " + secNum + "; " + "min = " + firstNum);
+
+// -------- Задача 2 ---------------
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.WriteLine("Введите первое число: ");
-int firstNum = Convert.ToInt32(Console.ReadLine());
+int N1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
-int secNum = Convert.ToInt32(Console.ReadLine());
+int N2 = Convert.ToInt32(Console.ReadLine());
 
-if (firstNum > secNum)
+Console.WriteLine("Введите третье число: ");
+int N3 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 > N2 && N1 > N3) 
 {
-    Console.WriteLine("max = " + firstNum + "; " + "min = " + secNum);
+      Console.WriteLine("max = " + N1);
 }
-else Console.WriteLine("max = " + secNum + "; " + "min = " + firstNum);
+else if (N2 > N1 && N2 > N3)
+{
+    Console.WriteLine("max = " + N2);
+}
+else Console.WriteLine("max = " + N3);
